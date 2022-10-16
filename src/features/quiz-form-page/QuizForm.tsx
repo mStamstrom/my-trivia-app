9f790702506d15d97d61d8d8bf03f7b5760ca201
Marcus Stamström <marcus.stamstrom@gmail.com>
2022-10-16 11:55:58 +0200

    Adding error message of amount is not filled in quiz form

--- src/features/quiz-form-page/QuizForm.tsx
+++ src/features/quiz-form-page/QuizForm.tsx
@@ -19,9 +19,13 @@ const difficultyOptions = ["Any difficulty", "Easy", "Medium", "Hard"];
 export const QuizForm = ({ categories, submit }: Props) => {
   const [amount, setAmount] = useState(0);
   const [difficultyLevel, setDifficultyLevel] = useState("");
+  const [showAmountError, setShowAmountError] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState<Category>();
 
   const onInputChange = (inputValue: number) => {
+    if (inputValue > 0) {
+      setShowAmountError(false);
+    }
     setAmount(inputValue);
   };
 
@@ -39,12 +43,21 @@ export const QuizForm = ({ categories, submit }: Props) => {
 
   const onSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    submit(amount, difficultyLevel, selectedCategory);
+    if (amount > 0) {
+      submit(amount, difficultyLevel, selectedCategory);
+    } else {
+      setShowAmountError(true);
+    }
   };
 
   return (
     <form onSubmit={onSubmit} className={styles.form}>
       <Input label="Amount" onInputChange={onInputChange} />
+      {showAmountError && (
+        <div style={{ color: "red", marginBottom: "1rem" }}>
+          Amount needs to be larger than 0
+        </div>
+      )}
       <Dropdown
         options={difficultyOptions}
         label="Difficulty level"
